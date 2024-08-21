2b6dc88418cbb17890200895a20a6aed5d89ffba
ericekirem <irem.ericek@icloud.com>
2024-08-21 11:52:01 +0300

    Add project files.

---
+++ Polymorphism/BaseGeometrikSeki.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class BaseGeometrikSekil
+    {
+        public double Genislik { get; set; }
+        public double Yukseklik { get; set; }
+
+        // Alan hesaplama metodu, türetilen sınıflarda override edilebilir
+        public virtual double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+
+    public class Kare : BaseGeometrikSekil
+    {
+        public Kare(double kenar)
+        {
+            Genislik = kenar;
+            Yukseklik = kenar;
+        }
+
+        // Kare için alan hesaplama
+        public override double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+
+    public class Dikdortgen : BaseGeometrikSekil
+    {
+        public Dikdortgen(double genislik, double yukseklik)
+        {
+            Genislik = genislik;
+            Yukseklik = yukseklik;
+        }
+
+        // Dikdörtgen için alan hesaplama
+        public override double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+
+    public class DikUcgen : BaseGeometrikSekil
+    {
+        public DikUcgen(double genislik, double yukseklik)
+        {
+            Genislik = genislik;
+            Yukseklik = yukseklik;
+        }
+
+        // Dik Üçgen için alan hesaplama
+        public override double AlanHesapla()
+        {
+            return (Genislik * Yukseklik) / 2;
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Polymorphism;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Polymorphism ile şekil nesneleri oluşturuluyor
+        BaseGeometrikSekil kare = new Kare(5);
+        BaseGeometrikSekil dikdortgen = new Dikdortgen(4, 7);
+        BaseGeometrikSekil dikUcgen = new DikUcgen(3, 6);
+
+        // Her bir nesnenin alanını hesaplayarak konsola yazdırıyoruz
+        Console.WriteLine($"Kare Alanı: {kare.AlanHesapla()}");
+        Console.WriteLine($"Dikdörtgen Alanı: {dikdortgen.AlanHesapla()}");
+        Console.WriteLine($"Dik Üçgen Alanı: {dikUcgen.AlanHesapla()}");
+    }
+}
